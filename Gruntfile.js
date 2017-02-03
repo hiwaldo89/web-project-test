dfed7f30b602972110b0826f8dc70745e32051a9
hiwaldo89 <hiwaldo89@gmail.com>
2017-02-02 18:43:33 -0600

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		/**
+		 * Sass task
+		 */
+		sass: {
+			dev: {
+				options: {
+					style: 'expanded',
+					sourcemap: 'none',
+				},
+				files: {
+					'style-expanded.css': 'sass/style.scss'
+				}
+			},
+			dist: {
+				options: {
+					style: 'compressed',
+					sourcemap: 'none',
+				},
+				files: {
+					'style.css': 'sass/style.scss'
+				}
+			}
+		},	
+		/**
+		 * Autoprefixer task
+		 */
+		 autoprefixer: {
+		 	options: {
+		 		browsers: ['last 2 versions']
+		 	},
+		 	// prefix all files
+		 	multiple_files: {
+		 		epand: true,
+		 		flatten: true,
+		 		src: 'compiled/*.css',
+		 		dest: ''
+		 	}
+		 },
+		/**
+		 * Watch task
+		 */
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass', 'autoprefixer']
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.registerTask('default', ['watch']);
+}
